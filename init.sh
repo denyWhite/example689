0101d881be1869c60ba27be2274f96a424b791cc
Denis Belov <den-belov@yandex.ru>
2018-11-12 18:35:50 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,2 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/site-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
