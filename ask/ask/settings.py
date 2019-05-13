8a167c204ce3d94372871ce56e57e36e6cf6ecf7
Denis Belov <den-belov@yandex.ru>
2019-05-13 14:31:59 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6l+zfbp5_#=@zu#rrusgd)ra-3bottp22yk=(1@9nd-#t-vxm='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
